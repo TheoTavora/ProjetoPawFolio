8603599ba0d2e203098245a028467b6c0ec2fb70
TheoTavora <theovannitavora@gmail.com>
2025-10-26 22:21:15 -0300

    comando do BD via django

--- pawfolio/core/models.py
+++ pawfolio/core/models.py
@@ -14,8 +14,8 @@ class Agendamento(models.Model):
     data_agendamento = models.DateField()
 
     class Meta:
-        managed = False
         db_table = 'agendamento'
+        managed = True
 
 
 class Cliente(models.Model):
@@ -25,11 +25,12 @@ class Cliente(models.Model):
     senha = models.CharField(max_length=100)
 
     class Meta:
-        managed = False
         db_table = 'cliente'
+        managed = True
 
 
 class Pet(models.Model):
+    id_pet = models.AutoField(primary_key=True)
     id_dono = models.ForeignKey(Cliente, models.DO_NOTHING, db_column='id_dono', blank=True, null=True)
     nome = models.CharField(max_length=100)
     comportamento = models.CharField(max_length=100, blank=True, null=True)
@@ -37,25 +38,32 @@ class Pet(models.Model):
     diferencial = models.CharField(max_length=100, blank=True, null=True)
 
     class Meta:
-        managed = False
         db_table = 'pet'
+        managed = True
 
 
 class Petshop(models.Model):
+    id_petshop = models.AutoField(primary_key=True)
     nome = models.CharField(max_length=100)
     email = models.CharField(max_length=100)
     senha = models.CharField(max_length=100)
+    rua = models.CharField(max_length=100, default='')
+    cidade = models.CharField(max_length=100, default='')
+    estado = models.CharField(max_length=100, default='')
+    telefone = models.CharField(max_length=15, default='', blank=True)
 
     class Meta:
-        managed = False
         db_table = 'petshop'
+        managed = True
 
 
 class Servicos(models.Model):
+    id_servico = models.AutoField(primary_key=True)
     pshop_related = models.ForeignKey(Petshop, models.DO_NOTHING, db_column='pshop_related', blank=True, null=True)
     descricao = models.CharField(max_length=100)
     preco = models.CharField(max_length=100)
+    duracao = models.CharField(max_length=100, blank=True, null=True)
 
     class Meta:
-        managed = False
         db_table = 'servicos'
+        managed = True
